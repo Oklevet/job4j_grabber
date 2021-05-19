6e2b13e24ab1a041d49486b38fa47f9bc067cad5
Oklevet <nikstar1235@gmail.com>
2021-05-19 16:57:12 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,35 +4,57 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.utils.SqlRuDateTimeParser;
-
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        List<String> altCol = new ArrayList<>();
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements col = doc.select(".altCol");
-        int count = 0;
-        for (Element td : col) {
-            if (count % 2 != 0) {
-                altCol.add(td.text());
-            }
-            count++;
-        }
-        count = 0;
 
-        for (Element tr : row) {
-            Element href = tr.child(0);
-            String date = altCol.get(count++);
-            SqlRuDateTimeParser srd = new SqlRuDateTimeParser();
-            System.out.println(srd.parse(date));
-            System.out.println(date);
-            System.out.println(href.text());
-            System.out.println(href.attr("href"));
-            System.out.println();
+    public static void parsePage(String link, String conditionRecs, String conditionDates) {
+        Document doc;
+        try {
+            doc = Jsoup.connect(String.valueOf(link)).get();
+            List<String> altCol = new ArrayList<>();
+            Elements Recs = doc.select(conditionRecs);
+            Elements Dates = doc.select(conditionDates);
+            int count = 0;
+            for (Element td : Dates) {
+                if (count % 2 != 0) {
+                    altCol.add(td.text());
+                }
+                count++;
+            }
+            count = 0;
+
+            for (Element tr : Recs) {
+                Element href = tr.child(0);
+                String date = altCol.get(count++);
+//                SqlRuDateTimeParser srd = new SqlRuDateTimeParser();
+//                System.out.println(srd.parse(date));
+                System.out.println(date);
+                System.out.println(href.text());
+                System.out.println(href.attr("href"));
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void parsePages(String link, int numPage, String conditionRecs, String conditionDates) {
+        int size = link.length();
+        for (int i = 1; i <= numPage; i++) {
+            link = link.replaceAll(link.substring(size - 1), "s");
+            link = link + "/" + i;
+            parsePage(link, conditionRecs, conditionDates);
+        }
+    }
+
+    public static void main(String[] args) {
+        String link             = new String("https://www.sql.ru/forum/job-offers");
+        String conditionRecs    = ".postslisttopic";
+        String conditionDates   = ".altCol";
+        int numPages            = 5;
+        parsePages(link, numPages, conditionRecs, conditionDates);
     }
 }
