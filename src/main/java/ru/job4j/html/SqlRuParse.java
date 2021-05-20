aabaf38856c8db785cc1fcbc2462617a90b65e5d
Oklevet <nikstar1235@gmail.com>
2021-05-20 10:57:00 +0300

    2.4. SqlRuParse [#285213 #158482]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,57 +4,86 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
+import ru.job4j.model.Post;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
+    private String linkPost;
 
-    public static void parsePage(String link, String conditionRecs, String conditionDates) {
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> posts = new ArrayList<>();
         Document doc;
-        try {
-            doc = Jsoup.connect(String.valueOf(link)).get();
-            List<String> altCol = new ArrayList<>();
-            Elements Recs = doc.select(conditionRecs);
-            Elements Dates = doc.select(conditionDates);
-            int count = 0;
-            for (Element td : Dates) {
-                if (count % 2 != 0) {
-                    altCol.add(td.text());
-                }
-                count++;
-            }
-            count = 0;
+        doc = Jsoup.connect(String.valueOf(link)).get();
+        Elements rec = doc.select(".postslisttopic");
 
-            for (Element tr : Recs) {
-                Element href = tr.child(0);
-                String date = altCol.get(count++);
-//                SqlRuDateTimeParser srd = new SqlRuDateTimeParser();
-//                System.out.println(srd.parse(date));
-                System.out.println(date);
-                System.out.println(href.text());
-                System.out.println(href.attr("href"));
-                System.out.println();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (Element tr : rec) {
+            Element href = tr.child(0);
+            linkPost = href.attr("href");
+            posts.add(detail(linkPost));
         }
+        return posts;
     }
 
-    public static void parsePages(String link, int numPage, String conditionRecs, String conditionDates) {
-        int size = link.length();
+    @Override
+    public Post detail(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+        Elements msg = doc.getElementsByClass("msgBody");
+        Elements dateMsg = doc.getElementsByClass("msgFooter");
+        String plot = "";
+        String datePost = "";
+
+        int count = 0;
+        for (Element td : msg) {
+            if (count == 1) {
+                plot = td.text();
+                break;
+            }
+            count++;
+        }
+
+        for (Element td : dateMsg) {
+            String[] footerMsg = td.text().split(" \\[");
+            datePost = footerMsg[0];
+            break;
+        }
+        return new Post(datePost, link,  plot);
+    }
+
+    public Integer getMaxPage(String link) throws IOException {
+        Document doc;
+        doc = Jsoup.connect(String.valueOf(link)).get();
+        Elements recs = doc.select(".sort_options");    //class[2]
+        Element pages = recs.last();                            //style on from 2
+        Element href = pages.child(0);
+        String[] nums = href.text().split(" Показываются темы");
+        String[] lastNum = nums[0].split("\\.\\. ");
+        return Integer.parseInt(lastNum[1]);
+    }
+
+    public Map<Integer, List<Post>> parsePages(String link, int numPage) throws IOException {
+        Map<Integer, List<Post>> book = new HashMap<>();
+        String etalonLink = link;
+        SqlRuParse srp1 = new SqlRuParse();
         for (int i = 1; i <= numPage; i++) {
-            link = link.replaceAll(link.substring(size - 1), "s");
-            link = link + "/" + i;
-            parsePage(link, conditionRecs, conditionDates);
+            link = String.format("%s%s%s", etalonLink, "/", i);
+            book.put(i, list(link));
         }
-    }
+        return book;
+   }
 
-    public static void main(String[] args) {
-        String link             = new String("https://www.sql.ru/forum/job-offers");
-        String conditionRecs    = ".postslisttopic";
-        String conditionDates   = ".altCol";
-        int numPages            = 5;
-        parsePages(link, numPages, conditionRecs, conditionDates);
+    public static void main(String[] args) throws IOException {
+        Map<Integer, List<Post>> book = new HashMap<>();
+        SqlRuParse srp      = new SqlRuParse();
+        String link         = "https://www.sql.ru/forum/job-offers";
+        int numPages        = 5;
+        int allPages        = srp.getMaxPage(link);
+        //System.out.println(allPages);             //количество страниц на форуме
+        book = srp.parsePages(link, numPages);           //тестовые первые пять страниц форума
+        //book = srp.parsePages(link, allPages);    //Полный парсинг
+        System.out.println(book.size());
     }
 }
