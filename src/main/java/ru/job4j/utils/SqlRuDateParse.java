1cfedef4d85268cdc86d6c51d94f24821f130cb5
Oklevet <nikstar1235@gmail.com>
2021-05-20 16:41:50 +0300

    2.1. Преобразование даты [#289476 #157975]

---
+++ src/main/java/ru/job4j/utils/SqlRuDateParse.java
@@ -0,0 +1,37 @@
+package ru.job4j.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqlRuDateParse {
+    public static void main(String[] args) throws Exception {
+        List<String> altCol = new ArrayList<>();
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        Elements col = doc.select(".altCol");
+        int count = 0;
+        for (Element td : col) {
+            if (count % 2 != 0) {
+                altCol.add(td.text());
+            }
+            count++;
+        }
+        count = 0;
+
+        for (Element tr : row) {
+            Element href = tr.child(0);
+            String date = altCol.get(count++);
+            SqlRuDateTimeParser srd = new SqlRuDateTimeParser();
+            System.out.println(srd.parse(date));
+            System.out.println(date);
+            System.out.println(href.text());
+            System.out.println(href.attr("href"));
+            System.out.println();
+        }
+    }
+}
--- src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
@@ -6,42 +6,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MM yy, HH:mm");
+    Map<String, String> months = new HashMap<>();
+        {
+            months.put("янв", "01");
+            months.put("фев", "02");
+            months.put("мар", "03");
+            months.put("апр", "04");
+            months.put("май", "05");
+            months.put("июн", "06");
+            months.put("июл", "07");
+            months.put("авг", "08");
+            months.put("сен", "09");
+            months.put("окт", "10");
+            months.put("ноя", "11");
+            months.put("дек", "12");
+        }
+
+    public int getMins(String str) {
+        int comma = str.indexOf(',');
+        return Integer.parseInt(str.substring(comma + 5, comma + 7));
+    }
+
+    public int getHours(String str) {
+        int comma = str.indexOf(',');
+        return Integer.parseInt(str.substring(comma + 2, comma + 4));
+    }
+
     @Override
     public LocalDateTime parse(String str) throws Exception {
-        Map<String, String> months = new HashMap<>();
-        months.put("янв", "01");
-        months.put("фев", "02");
-        months.put("мар", "03");
-        months.put("апр", "04");
-        months.put("май", "05");
-        months.put("июн", "06");
-        months.put("июл", "07");
-        months.put("авг", "08");
-        months.put("сен", "09");
-        months.put("окт", "10");
-        months.put("ноя", "11");
-        months.put("дек", "12");
-
-        int comma = 0, hour, mins;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MM yy, HH:mm");
+        Character firstSym = str.charAt(0);
         LocalDateTime dayNow = LocalDateTime.now();
 
-        if (Character.isDigit(str.charAt(0))) {
+        if (Character.isDigit(firstSym)) {
             String mon = str.substring(str.indexOf(" ") + 1, str.indexOf(" ", str.indexOf(" ") + 1));
             return LocalDateTime.parse(str.replaceAll(mon, months.get(mon)), formatter);
         } else {
-            switch (str.charAt(0)) {
+            switch (firstSym) {
                 case ('с'):
-                    comma = str.indexOf(',');
-                    hour = Integer.parseInt(str.substring(comma + 2, comma + 4));
-                    mins = Integer.parseInt(str.substring(comma + 5, comma + 7));
-                    System.out.println(hour + " " + mins);
-                    return LocalDateTime.of(dayNow.getYear(), dayNow.getMonth(), dayNow.getDayOfMonth(), hour, mins);
+                    return LocalDateTime.of(dayNow.getYear(), dayNow.getMonth(), dayNow.getDayOfMonth(),
+                            getHours(str), getHours(str));
                 case ('в'):
-                    comma = str.indexOf(',');
-                    hour = Integer.parseInt(str.substring(comma + 2, comma + 4));
-                    mins = Integer.parseInt(str.substring(comma + 5, comma + 7));
-                    return LocalDateTime.of(dayNow.getYear(), dayNow.getMonth(), dayNow.getDayOfMonth() - 1, hour, mins);
+                    return LocalDateTime.of(dayNow.getYear(), dayNow.getMonth(), dayNow.getDayOfMonth() - 1,
+                            getHours(str), getMins(str));
                 default:
                     throw new Exception("wrong format of date");
             }
